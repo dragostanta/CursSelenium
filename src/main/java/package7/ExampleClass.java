4b1b7f502bf3c1babe485dd3343e1c5556ac3311
Dragos Tanta <dragos.tanta@gmail.com>
2022-07-05 22:37:27 +0300

    curs 7

---
+++ src/main/java/package7/ExampleClass.java
@@ -0,0 +1,21 @@
+package package7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ExampleClass {
+	
+	@Test
+	public void testCeva() {
+		//System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		System.out.println("Print test!");
+		WebDriver driver =  new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.close();
+		
+	}
+	
+	
+
+}
